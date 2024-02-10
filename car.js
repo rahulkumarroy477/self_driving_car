6497dee386d31f0806d9576f95a394b28b413882
Rahul kumar <110653984+rahulkumarroy477@users.noreply.github.com>
2024-02-10 07:18:05 +0530

    adding sensors

--- car.js
+++ car.js
@@ -1,52 +1,67 @@
 class Car{
     constructor(x,y,width,height){
-        this.x = x;
-        this.y = y;
-        this.width = width;
-        this.height = height;
+        this.x=x;
+        this.y=y;
+        this.width=width;
+        this.height=height;
 
-        this.speed = 0;
-        this.acceleration = 0.2;
-        this.maxSpeed = 3;
-        this.friction = 0.05;
-        this.angle = 0;     // unitcircle for rotation
+        this.speed=0;
+        this.acceleration=0.2;
+        this.maxSpeed=3;
+        this.friction=0.05;
+        this.angle=0;
 
-        this.controls = new Controls();
+        this.sensor=new Sensor(this);
+        this.controls=new Controls();
     }
-    upadate(){
+
+    update(roadBorders){
         this.#move();
+        this.sensor.update(roadBorders);
     }
+
     #move(){
         if(this.controls.forward){
-            this.speed += this.acceleration;
+            this.speed+=this.acceleration;
         }
         if(this.controls.reverse){
-            this.speed -= this.acceleration;
+            this.speed-=this.acceleration;
         }
-        if(this.speed>this.maxSpeed)    this.speed = this.maxSpeed;
-        if(this.speed<-this.maxSpeed/2){    // -speed for car going backward
-            this.speed =- this.maxSpeed/2;
-        }
-        if(this.speed>0)    this.speed -= this.friction;
-        if(this.speed<0)    this.speed += this.friction;
-        if(Math.abs(this.speed)<this.friction)  this.speed = 0;
 
-        // left and right movement should be reversed in case of reverse
+        if(this.speed>this.maxSpeed){
+            this.speed=this.maxSpeed;
+        }
+        if(this.speed<-this.maxSpeed/2){
+            this.speed=-this.maxSpeed/2;
+        }
+
+        if(this.speed>0){
+            this.speed-=this.friction;
+        }
+        if(this.speed<0){
+            this.speed+=this.friction;
+        }
+        if(Math.abs(this.speed)<this.friction){
+            this.speed=0;
+        }
+
         if(this.speed!=0){
-            const flip = this.speed>0?1:-1;
+            const flip=this.speed>0?1:-1;
             if(this.controls.left){
-                this.angle += 0.03;
+                this.angle+=0.03*flip;
             }
             if(this.controls.right){
-                this.angle -= 0.03;
+                this.angle-=0.03*flip;
             }
         }
-        this.x -= Math.sin(this.angle)*this.speed;
-        this.y -= Math.cos(this.angle)*this.speed;
+
+        this.x-=Math.sin(this.angle)*this.speed;
+        this.y-=Math.cos(this.angle)*this.speed;
     }
+
     draw(ctx){
         ctx.save();
-        ctx.translate(this.x, this.y);
+        ctx.translate(this.x,this.y);
         ctx.rotate(-this.angle);
 
         ctx.beginPath();
@@ -57,6 +72,9 @@ class Car{
             this.height
         );
         ctx.fill();
+
         ctx.restore();
+
+        this.sensor.draw(ctx);
     }
 }
